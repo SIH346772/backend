bc5fb33d579187a7dc03327c4e7d3585dcddedb1
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-09-18 11:43:19 +0530

    feat: added send sms function

---
+++ utils/communication.js
@@ -0,0 +1,27 @@
+const axios = require("axios").default;
+
+/**
+    * Send OTP to mobile number
+    * 
+    * @async
+    * @function
+    * @param {string} mobile - mobile number
+    * @param {Number} otp - otp to send
+    * @return {boolean} true if success, false if failed
+*/
+const sendOTP = async (mobile, otp) => {
+    try {
+        const request = await axios.post("https://www.fast2sms.com/dev/bulkV2?authorization="+process.env.FAST2SMS_API_KEY+"&route=otp&variables_values=&"+otp.toString()+"flash=0&numbers="+mobile);
+        if(request.status == 200) {
+            return true;
+        }
+        return false;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+module.exports = {
+    sendOTP
+}
